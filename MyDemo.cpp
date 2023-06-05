cb358a8c0a24ae50caf08537cf3076d7277d21fb
roey-ubuntu <roey.fenigold@gmail.com>
2023-06-05 12:59:45 +0300

    MyDemo.cpp: update

--- MyDemo.cpp
+++ MyDemo.cpp
@@ -6,47 +6,47 @@ using namespace ariel;
 int main() {
     // Create a MagicalContainer and add some elements
     MagicalContainer my_container;
-    cout << "my_container: " << my_container <<"\t\t\t\tsize: " << my_container.size() << "\n\n";
+    cout << "my_container: " << my_container << "\t\t\t\tsize: " << my_container.size() << "\n";
 
-    cout << "add 17" << "\n";
+    cout << "\tadd 17" << "\n";
     my_container.addElement(17);
-    cout << "my_container: " << my_container <<"\t\t\t\tsize: " << my_container.size() << "\n\n";
+    cout << "my_container: " << my_container << "\t\t\t\tsize: " << my_container.size() << "\n";
 
-    cout << "add 2" << "\n";
+    cout << "\tadd 2" << "\n";
     my_container.addElement(2);
-    cout << "my_container: " << my_container <<"\t\t\t\tsize: " << my_container.size() << "\n\n";
+    cout << "my_container: " << my_container << "\t\t\t\tsize: " << my_container.size() << "\n";
 
-    cout << "add 25" << "\n";
+    cout << "\tadd 25" << "\n";
     my_container.addElement(25);
-    cout << "my_container: " << my_container <<"\t\t\tsize: " << my_container.size() << "\n\n";
+    cout << "my_container: " << my_container << "\t\t\tsize: " << my_container.size() << "\n";
 
-    cout << "add 9" << "\n";
+    cout << "\tadd 9" << "\n";
     my_container.addElement(9);
-    cout << "my_container: " << my_container <<"\t\t\tsize: " << my_container.size() << "\n\n";
+    cout << "my_container: " << my_container << "\t\t\tsize: " << my_container.size() << "\n";
 
-    cout << "add 3" << "\n";
+    cout << "\tadd 3" << "\n";
     my_container.addElement(3);
-    cout << "my_container: " << my_container <<"\t\t\tsize: " << my_container.size() << "\n\n";
+    cout << "my_container: " << my_container << "\t\t\tsize: " << my_container.size() << "\n";
 
-    cout << "add -2" << "\n";
+    cout << "\tadd -2" << "\n";
     my_container.addElement(-2);
-    cout << "my_container: " << my_container <<"\t\tsize: " << my_container.size() << "\n\n";
+    cout << "my_container: " << my_container << "\t\tsize: " << my_container.size() << "\n";
 
-    cout << "add 122" << "\n";
+    cout << "\tadd 122" << "\n";
     my_container.addElement(122);
-    cout << "my_container: " << my_container <<"\t\tsize: " << my_container.size() << "\n\n";
+    cout << "my_container: " << my_container << "\t\tsize: " << my_container.size() << "\n";
 
-    cout << "add -13" << "\n";
+    cout << "\tadd -13" << "\n";
     my_container.addElement(-13);
-    cout << "my_container: " << my_container <<"\tsize: " << my_container.size() << "\n\n";
+    cout << "my_container: " << my_container << "\tsize: " << my_container.size() << "\n";
 
-    cout << "add 44" << "\n";
+    cout << "\tadd 44" << "\n";
     my_container.addElement(44);
-    cout << "my_container: " << my_container <<"\tsize: " << my_container.size() << "\n\n";
+    cout << "my_container: " << my_container << "\tsize: " << my_container.size() << "\n";
 
-    cout << "add 26" << "\n";
+    cout << "\tadd 26" << "\n";
     my_container.addElement(26);
-    cout << "my_container: " << my_container <<"\tsize: " << my_container.size() << "\n\n";
+    cout << "my_container: " << my_container << "\tsize: " << my_container.size() << "\n";
 
 
 
@@ -56,7 +56,9 @@ int main() {
     for (auto it = ascIter.begin(); it != ascIter.end(); ++it) {
         std::cout << *it << ' ';   // 2 3 9 17 25
     }
-    std::cout << std::endl;
+    std::cout << "\n\n" << std::endl;
+
+
 
     // Use DescendingIterator to display elements in descending order
     std::cout << "Elements in cross order:\n";
@@ -64,7 +66,9 @@ int main() {
     for (auto it = crossIter.begin(); it != crossIter.end(); ++it) {
         std::cout << *it << ' ';  // 2 25 3 17 9
     }
-    std::cout << std::endl;
+    std::cout << "\n\n" << std::endl;
+
+
 
     // Use PrimeIterator to display prime numbers only
     std::cout << "Prime numbers:\n";
@@ -72,7 +76,7 @@ int main() {
     for (auto it = primeIter.begin(); it != primeIter.end(); ++it) {
         std::cout << *it << ' ';  // 2 3 17 
     }
-    std::cout << std::endl;
+    std::cout << "\n" << std::endl;
 
     // Remove an element from the my_container and display the size
     my_container.removeElement(9);
